148ab4ff066d6a7fbf64f1c4383b6b4b401f07cf
Lavínia Winter <105389999+KopWinter@users.noreply.github.com>
2022-09-29 09:55:10 -0300

    Commit pt. 3

---
+++ App.java
@@ -0,0 +1,10 @@
+package pucrs.myflight.consoleApp;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("\nMyFlight project...");
+	MyFlight mf1 = new MyFlight();
+	mf1.inicializa();
+	}
+}
